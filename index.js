5dc0dda69456b6cba9b8a5a24f78ecdde2d3c7ac
axis <axisotherwise@gmail.com>
2022-09-15 03:15:28 +0900

    Update

--- index.js
+++ index.js
@@ -3,30 +3,34 @@ const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
-  const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length - 1];
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg 사용합니다.
-  console.log('name', filename, 'ext', ext);
+exports.handler = async (event, context, done) => {
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = event.Records[0].s3.object.key;
 
-  try {
-    const s3Object = await s3.getObject({ Bucket, Key }).promise(); // 버퍼로 가져오기
-    console.log('original', s3Object.Body.length);
-    const resizedImage = await sharp(s3Object.Body) // 리사이징
-      .resize(200, 200, { fit: 'inside' })
-      .toFormat(requiredFormat)
-      .toBuffer();
-    await s3.putObject({ // thumb 폴더에 저장
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: resizedImage,
-    }).promise();
-    console.log('put', resizedImage.length);
-    return callback(null, `thumb/${filename}`);
-  } catch (error) {
-    console.error(error);
-    return callback(error);
-  }
+    const filename = Key.split("/")[Key.split("/").length - 1];
+    const ext = Key.split(".")[Key.split(".").length - 1];
+    const format = ext === "jpg" ? "jpeg" : ext;
+
+    try {
+        // 이미지 버퍼
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
+
+        // 리사이징 옵션
+        const resizedImage = await sharp(s3Object.Body)
+        .resize(600, 600, { fit: "inside" })
+        .toFormat(format)
+        .toBuffer();
+        await s3.putObject(
+            {
+                Bucket,
+                Key: `comment-resized/${filename}`, // 저장 경로
+                Body: resizedImage,
+            }
+        ).promise();
+
+        return done(null, `comment-resized/${filename}`);
+    } catch (error) {
+        console.error(error);
+        return done(error);
+    }
 };
