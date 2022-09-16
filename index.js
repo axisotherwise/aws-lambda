b75348412a45d69bf2095989fbe5b75f8f13793a
axis <axisotherwise@gmail.com>
2022-09-16 13:59:12 +0900

    Update comment resizing

--- index.js
+++ index.js
@@ -3,34 +3,30 @@ const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-exports.handler = async (event, context, done) => {
+exports.handler = async (event, context, callback) => {
     const Bucket = event.Records[0].s3.bucket.name;
     const Key = event.Records[0].s3.object.key;
 
-    const filename = Key.split("/")[Key.split("/").length - 1];
-    const ext = Key.split(".")[Key.split(".").length - 1];
-    const format = ext === "jpg" ? "jpeg" : ext;
+    const filename = Key.split('/')[Key.split('/').length - 1];
+    const ext = Key.split('.')[Key.split('.').length - 1];
+    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
 
     try {
-        // 이미지 버퍼
         const s3Object = await s3.getObject({ Bucket, Key }).promise();
 
-        // 리사이징 옵션
         const resizedImage = await sharp(s3Object.Body)
-        .resize(400, 400, { fit: "inside" })
-        .toFormat(format)
+        .resize(600, 600, { fit: 'inside' })
+        .toFormat(requiredFormat)
         .toBuffer();
-        await s3.putObject(
-            {
-                Bucket,
-                Key: `comment-resized/${filename}`, // 저장 경로
-                Body: resizedImage,
-            }
-        ).promise();
+        await s3.putObject({
+        Bucket,
+        Key: `thumb/${filename}`,
+        Body: resizedImage,
+        }).promise();
 
-        return done(null, `comment-resized/${filename}`);
+        return callback(null, `comment-resized/${filename}`);
     } catch (error) {
         console.error(error);
-        return done(error);
+        return callback(error);
     }
 };
